301683c1a7d8c9250be79bca009edbfa3e6aaad5
SkillerChampion <128652723+SkillerChampion@users.noreply.github.com>
2023-04-30 05:21:45 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 const { initializePgConnection, loadBatisMappers } = require('./src/utils/database/database');
 
 loadBatisMappers();
@@ -12,6 +13,14 @@ app.use(express.json({ extended: false }));
 app.get('/api/health', (req, res) => res.send('BE Hedera app is running'));
 
 app.use('/api/hederaService', require('./src/routes/hederaService'));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 const PORT = 8080;
 
